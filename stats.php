1a32f60e269a00c1e1fa37a38ebd4391990a295f
Callum Bryson <callumjbryson@gmail.com>
2018-10-18 00:58:33 +1000

    stats css fix

--- stats.php
+++ stats.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/Chart.min.js"></script>
 
